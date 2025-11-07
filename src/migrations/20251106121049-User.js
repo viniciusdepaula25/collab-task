78472e091d0005c0977a4d6f99c224a076b4ca33
viniciusdepaula25 <viniciuspaulaesilva@gmail.com>
2025-11-07 17:16:33 -0300

    Add migrate/model user

--- src/migrations/20251106121049-User.js
+++ src/migrations/20251106121049-User.js
@@ -1,22 +1,47 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+const { NOT } = require('sequelize/lib/deferrable');
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
