ca49e35f288dc876a8862d99db1f24b35de1000e
viniciusdepaula25 <viniciuspaulaesilva@gmail.com>
2025-11-24 17:18:24 -0300

    autentificação por token jwt

--- src/env.ts
+++ src/env.ts
@@ -1,27 +1,27 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
-import 'dotenv/config'
+import 'dotenv/config';
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'test', 'prodution']). default('prodution'),
-    //DATABASE_URL: z.string(),
-    PORT: z.coerce.number().default(3000),
-    JWT_SECRET: z.string(),
-    DB_NAME: z.string(),
-    DB_USER: z.string(),
-    DB_PASSWORD: z.string(),
-    DB_DIALECT: z.string(),
-    DB_HOST: z.string(),
-    DB_PORT: z.coerce.number(),
+  NODE_ENV: z.enum(['development', 'test', 'prodution']). default('prodution'),
+  //DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3000),
+  JWT_SECRET: z.string(),
+  DB_NAME: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_DIALECT: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.coerce.number(),
 
-})
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-    console.error('Invalid environment variables!', _env.error.format())
+  console.error('Invalid environment variables!', _env.error.format());
 
-    throw new Error('Invalid environment variables!')
+  throw new Error('Invalid environment variables!');
 }
 
-export const env = _env.data
+export const env = _env.data;
---
+++ src/middlewares/authenticated.ts
@@ -0,0 +1,26 @@
+import {Request, Response, NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+import {env} from 'src/env';
+
+export async function authorized(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const autHeader = req.headers.authorization;
+  const token = autHeader && autHeader.split(' ')[1];
+
+  if(!token) throw new Error('Não autorizado');
+
+  try {
+    const decodeToken = jwt.verify(token, env.JWT_SECRET);
+
+    const {id} = decodeToken as {id: number};
+
+    req.users = {id};
+
+    next();
+  }catch (err) {
+    throw new Error('Não autorizado');
+  }
+}
