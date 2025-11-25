4932b6c4ab970cbd2d8158e59d6ef55c0b238804
viniciusdepaula25 <viniciuspaulaesilva@gmail.com>
2025-11-25 10:42:51 -0300

    fix: prefixo rota user

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { userRoutes } from './modules/users/routes/users.routes';
 
 export const routes = express.Router();
 
-routes.use('/users', userRoutes);
+routes.use('/user', userRoutes);
 
 
 
